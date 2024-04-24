8f6cddd25318ab833b4be2946406b8703618105d
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-04-23 22:24:47 -0300

    afaF

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
       <div className="credits-button-container">
         <div className="credits-button">CREDITS</div>
         <div className="credits-container">
-          <span>Fonts are Road Rage, Neue Montreal, and 2049. Music by Ivymusic and Looptape on Pixabay. Background image by Franz26 on Pixabay. Personal use only. No money is being made through this site.</span>
+          <span>Fonts are Road Rage, Neue Montreal, and 2049. Music by Ivymusic on Pixabay. Background image by Franz26 on Pixabay. Personal use only. No money is being made through this site.</span>
         </div>
       </div>
     </div>

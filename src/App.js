8719af75604ca08b8d6592e28060bab6d146347f
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-07-09 21:07:58 -0300

    Afa

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
       <div className="credits-button-container">
         <div className="credits-button">CREDITS</div>
         <div className="credits-container">
-          <span>Fonts are Alagard, Migha, Kayak, and Neue Montreal. Music by Roman Senyk from Pixabay. Background image by Franz26 from Pixabay. Personal use only. No money is being made through this site.</span>
+          <span>Fonts are Alagard, Ragna, Kayak, and Neue Montreal. Music by Roman Senyk from Pixabay. Background image by Franz26 from Pixabay. Personal use only. No money is being made through this site.</span>
         </div>
       </div>
     </div>

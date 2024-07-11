b13ddc909caa844c7c0971765c3f0aac3261849f
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-07-10 22:11:38 -0300

    afa

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,108 +1,74 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { renderTextWithSpan } from "../utils";
 
 import "../component_styles/AboutMe.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const AboutMe = () => {
-  const heroRef = useRef(null);
-  const waveTextRef = useRef(null);
-  const waveTextRef2 = useRef(null);
-  const paragraphRef = useRef(null);
+  const effect1Ref1= useRef(null);
+  const effect1Ref2 = useRef(null);
+  const effect2Ref1 = useRef(null);
+  const effect2Ref2 = useRef(null);
 
   useEffect(() => {
-    gsap.set([paragraphRef.current, waveTextRef.current, waveTextRef2.current], { autoAlpha: 0 });
+    gsap.set([effect1Ref1.current, effect1Ref2.current, effect2Ref1.current, effect2Ref2.current], { autoAlpha: 0 });
 
-    gsap.fromTo(
-      heroRef.current.querySelectorAll(".letter"),
-      { autoAlpha: 0, y: 20 },
-      {
-        duration: 1.5,
-        autoAlpha: 1,
-        y: 0,
-        stagger: 0.1,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: heroRef.current,
-          start: "top 50%",
-          end: "bottom top",
-          toggleActions: "play none none reverse",
-        },
-      }
-    );
+    const animate1 = (element) => {
+      gsap.fromTo(
+        element,
+        { autoAlpha: 0, x: -100, rotation: -10, scale: 0.8 },
+        {
+          duration: 1.5, autoAlpha: 1, x: 0, rotation: 0, scale: 1, ease: "power2.out",
+          scrollTrigger: {
+            trigger: element,
+            start: "top 40%",
+            end: "bottom top",
+            toggleActions: "play none none reverse",
+          }
+        }
+      );
+    };
 
-    gsap.fromTo(
-      paragraphRef.current,
-      { autoAlpha: 0, y: 20 },
-      {
-        duration: 1.5,
-        autoAlpha: 1,
-        y: 0,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: paragraphRef.current,
-          start: "top 75%",
-          end: "bottom top",
-          toggleActions: "play reverse play reverse",
-        },
-      }
-    );
+    const animate2 = (element) => {
+      gsap.fromTo(
+        element,
+        { autoAlpha: 0 },
+        {
+          duration: 1.2, autoAlpha: 1, ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: element,
+            start: "top 40%",
+            end: "top top",
+            toggleActions: "play none none reverse",
+          }
+        }
+      );
+    };
 
-    gsap.fromTo(
-      waveTextRef2.current,
-      { autoAlpha: 0, y: 20 },
-      {
-        duration: 1.5,
-        autoAlpha: 1,
-        y: 0,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: waveTextRef2.current,
-          start: "top 75%",
-          end: "bottom top",
-          toggleActions: "play reverse play reverse",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      waveTextRef.current,
-      { autoAlpha: 0, y: 20 },
-      {
-        duration: 1.5,
-        autoAlpha: 1,
-        y: 0,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: waveTextRef.current,
-          start: "top 75%",
-          end: "bottom top",
-          toggleActions: "play reverse play reverse",
-        },
-      }
-    );
+    animate1(effect1Ref1.current);
+    animate1(effect1Ref2.current);
+    animate2(effect2Ref1.current);
+    animate2(effect2Ref2.current);
   }, []);
 
   return (
-    <div ref={heroRef} className="AboutMe">
-      <h1 className="Nate">
-      {renderTextWithSpan("My name is Nate... ", "letter")}
-
+    <div className="AboutMe">
+      <h1 ref={effect1Ref1} className="Nate">
+        My name is Nate...
       </h1>
-      <div ref={paragraphRef} className="AboutMe">
+      <div ref={effect1Ref2} className="Intro">
         <span>I'm a full stack web developer from Miramichi, Canada.</span>
         <span> I bring a wide range of front and back end skills to the table,</span>
         <span> and a relentless pursuit of perfection.</span>
       </div>
-      <div ref={waveTextRef2} className="FullStack">
-        I try to add a litle bit of magic to web development...
+      <div ref={effect2Ref1} className="FullStack">
+        I try to add a little bit of magic to web development...
       </div>
-      <div ref={waveTextRef} className="Bold-Immersive-Memorable">
-      with Modern, Memorable, and Immersive design that hits hard.
-       </div>
+      <div ref={effect2Ref2} className="Bold-Immersive-Memorable">
+        with Modern, Memorable, and Immersive design.
+      </div>
     </div>
   );
 };

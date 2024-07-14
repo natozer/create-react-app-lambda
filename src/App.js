ec0f4bc4b47c0ca793dd531c8f60613c2fe04dd4
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-07-14 16:05:03 -0300

    A

--- src/App.js
+++ src/App.js
@@ -1,75 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import './App.css';
-import { gsap } from 'gsap';
-import SnowScene from './components/SnowScene';
-import Header from './components/Header';
-import ContactMe from './components/Contact';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
-import SplashScreen from './components/SplashScreen';
-import backgroundMusic from './assets/cinematic-fantasy-dark.mp3';
-import AboutMe from './components/AboutMe';
-import Experience from './components/Experience';
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [splashScreenVisible, setSplashScreenVisible] = useState(true);
-  const [showContact, setShowContact] = useState(false);
 
-  const audioRef = useRef(new Audio(backgroundMusic));
-  audioRef.current.loop = true;
-
-  const splashScreenRef = useRef(null);
-
-  const toggleMusic = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleContactClick = () => {
-    setShowContact(!showContact);
-  };
-
-  const handleEnterSite = () => {
-    if (!isPlaying) {
-      toggleMusic();
-    }
-    gsap.to(splashScreenRef.current, { duration: 1, opacity: 0, onComplete: () => setSplashScreenVisible(false) });
-  };
-
-  useEffect(() => {
-    const currentAudio = audioRef.current;
-
-    return () => {
-      currentAudio.pause();
-    };
-  }, []);
 
   return (
     <div className="App">
-      <SnowScene />
-      {splashScreenVisible ? (
-        <SplashScreen onEnterSite={handleEnterSite} />
-      ) : (
-        <>
-          <Header isPlaying={isPlaying} toggleMusic={toggleMusic} onContactClick={handleContactClick} />
-          <Hero/>
-          <AboutMe/>
-          <Experience/>
-          {showContact && <ContactMe setShowContact={setShowContact} />}
-          <Footer />
-        </>
-      )}
-      <div className="credits-button-container">
-        <div className="credits-button">CREDITS</div>
-        <div className="credits-container">
-          <span>Fonts are Ragna, Kayak, Morven, and Neue Montreal. Music by Roman Senyk from Pixabay. Background image by Franz26 from Pixabay. This site is a personal concept demo.  No money is being made through this site.</span>
-        </div>
-      </div>
+    <h1>nothing to see here</h1>
     </div>
   );
 }
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const renderTextWithSpan = (text, className) => {
-  return text.split('').map((char, index) => (
-    <span key={index} className={className}>
-      {char}
-    </span>
-  ));
-};

950babd64ff438487c1d843ad04c449b4bcc48fa
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-07-29 18:18:23 -0300

    lol

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { ReactComponent as MuteIcon } from '../assets/mute.svg';
-import { ReactComponent as UnmuteIcon } from '../assets/unmute.svg';
-import { ReactComponent as EmailIcon } from '../assets/email.svg';
-import '../component_styles/AppHeader.css';
+import React, { useRef } from "react";
+import { ReactComponent as MuteIcon } from "../assets/mute.svg";
+import { ReactComponent as UnmuteIcon } from "../assets/unmute.svg";
+import { ReactComponent as EmailIcon } from "../assets/email.svg";
+import "../component_styles/AppHeader.css";
 
 function MusicVisualizer({ isPlaying }) {
   return (
-    <div className={`visualizer ${isPlaying ? 'playing' : ''}`}>
+    <div className={`visualizer ${isPlaying ? "playing" : ""}`}>
       {Array.from({ length: 5 }).map((_, index) => (
-        <div key={index} className='bar'></div>
+        <div key={index} className="bar"></div>
       ))}
     </div>
   );
@@ -16,7 +16,7 @@ function MusicVisualizer({ isPlaying }) {
 
 function ToggleButton({ isPlaying, toggleMusic }) {
   return (
-    <button onClick={toggleMusic} aria-label={isPlaying ? 'Mute' : 'Unmute'}>
+    <button onClick={toggleMusic} aria-label={isPlaying ? "Mute" : "Unmute"}>
       {isPlaying ? <MuteIcon /> : <UnmuteIcon />}
     </button>
   );
@@ -24,47 +24,25 @@ function ToggleButton({ isPlaying, toggleMusic }) {
 
 function Header({ isPlaying, toggleMusic }) {
   const headerRef = useRef(null);
-  const [iconColor, setIconColor] = useState('initialColor'); 
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const backgroundColor = entry.target.getAttribute('data-bg-color');
-            setIconColor(backgroundColor === 'dark' ? 'lightColor' : 'darkColor');
-          }
-        });
-      },
-      { threshold: 0.5 } 
-    );
-
-    const sections = document.querySelectorAll('.background-section');
-    sections.forEach((section) => observer.observe(section));
-
-    return () => {
-      sections.forEach((section) => observer.unobserve(section));
-    };
-  }, []);
 
   const renderTextWithSpan = (text) => {
-    return text.split('').map((char, index) => (
-      <span key={index} className='vertical-stack'>
+    return text.split("").map((char, index) => (
+      <span key={index} className="vertical-stack">
         {char}
       </span>
     ));
   };
 
   return (
-    <div className='header-container'>
-      <header className='App-header' ref={headerRef}>
-        <div className='header-left'>
-          {renderTextWithSpan('NATHANIEL TOZER')}
+    <div className="header-container">
+      <header className="App-header" ref={headerRef}>
+        <div className="header-left">
+          {renderTextWithSpan("NATHANIEL TOZER")}
         </div>
-        <div className='header-right'>
+        <div className="header-right">
           <MusicVisualizer isPlaying={isPlaying} />
           <ToggleButton isPlaying={isPlaying} toggleMusic={toggleMusic} />
-          <a href='mailto:natozer@gmail.com' className={`mail ${iconColor}`}>
+          <a href="mailto:natozer@gmail.com">
             <EmailIcon />
           </a>
         </div>
--- src/components/ThemeStation.jsx
+++ src/components/ThemeStation.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
-import '../component_styles/ThemeStation.css';
+import React, { useState } from "react";
+import "../component_styles/ThemeStation.css";
 
 const themes = [
-  { name: 'Millenial Gray', primary: 'var(--swedish-yellow)', secondary: 'var(--swedish-blue)' },
-  { name: 'Cyberpunk', primary: 'var(--icey-blue)', secondary: 'var(--burgundy)' },
-  { name: 'Flamingo', primary: 'var(--pink)', secondary: 'var(--teal)' },
-  { name: 'Miramichi Gothic', primary: 'var(--white)', secondary: 'var(--main-black)' },
-
+  {
+    name: "Millenial Gray",
+    primary: "var(--millenial-gray)",
+    secondary: "var(--pure-black)",
+  },
+  {
+    name: "Cyberpunk",
+    primary: "var(--icey-blue)",
+    secondary: "var(--burgundy)",
+  },
+  { name: "Flamingo", primary: "var(--pink)", secondary: "var(--teal)" },
+  {
+    name: "Miramichi Gothic",
+    primary: "var(--white)",
+    secondary: "var(--main-black)",
+  },
 ];
 
 const ThemeStation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleThemeChange = (theme) => {
-    document.documentElement.style.setProperty('--primary-color', theme.primary);
-    document.documentElement.style.setProperty('--secondary-color', theme.secondary);
-    
-    const event = new CustomEvent('themeChange');
+    document.documentElement.style.setProperty(
+      "--primary-color",
+      theme.primary
+    );
+    document.documentElement.style.setProperty(
+      "--secondary-color",
+      theme.secondary
+    );
+
+    const event = new CustomEvent("themeChange");
     window.dispatchEvent(event);
   };
 
   return (
-    <div className={`ThemeStation ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+    <div
+      className={`ThemeStation ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOpen(!isOpen)}
+    >
       {isOpen ? (
         <ul>
           {themes.map((theme, index) => (

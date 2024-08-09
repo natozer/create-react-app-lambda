3762c94ebe68f1b01d33728cc3a2cc1ba7b4da47
DESKTOP-3IF8SRJ\natoz <natozer@gmail.com>
2024-08-09 12:13:33 -0300

    a

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -6,7 +6,7 @@ const Credits = () => {
     <div className="credits-button-container">
       <div className="credits-button">CREDITS</div>
       <div className="credits-container">
-        <span>Fonts are Neue Montreal, Space Mono, and Road Rage. Music by Fronbonni_Skegs from Pixabay. Sound effect by BRVHRTZ from Pixabay. <br/>This site is a personal demo project. No money is being made through this site.</span>
+        <span>Fonts are Neue Montreal, Space Mono, and Road Rage. Music by Freccero from Pixabay. Sound effect by BRVHRTZ from Pixabay. <br/>This site is a personal demo project. No money is being made through this site.</span>
       </div>
     </div>
   );
